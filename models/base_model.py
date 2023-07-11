f5aa0530a8798a9eb888104639f7cccedcbb4a0c
Modeste Ciira <ciiramodeste23@gmail.com>
2023-07-11 12:03:26 +0300

    Its just the beginning

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,26 @@
 """defines a class BaseModel"""
 
 import uuid
-import datetime
+from datetime import datetime
 
-formated_time = specific_datetime.strftime("%Y-%m-%d %H:%M:%S")
+"""formated_time = specific_datetime.strftime("%Y-%m-%d %H:%M:%S:%f")"""
 
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        instance_dict = self.__dict__.copy()
+        instance_dict["__class__"] = self.__class__.__name__
+        instance_dict["created_at"] = self.created_at.isoformat()
+        instance_dict["updated_at"] = self.updated_at.isoformat()
+        return instance_dict
